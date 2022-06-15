74956a492ce18ef3a68eff4f7d67ef8c9f086559
ckihereko <102221386+ckihereko@users.noreply.github.com>
2022-06-15 16:39:56 +0300

    Update string_token.c

--- string_token.c
+++ string_token.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * *nbr_spaces - return the number of occurent of a string.
+ * *nbr_spaces - return the number of occurent of a string
  * @s: string to check
  * Return: int
  */
